20d1d1426193c45713ceffb50d46f300aa10ac1c
nasriibrahim2007 <nasriibrahim2007@gmail.com>
2026-07-05 14:21:12 +0300

    SW vide + register force update + cache clear

--- public/sw.js
+++ public/sw.js
@@ -1,2 +1,11 @@
-self.addEventListener('install', function() { self.skipWaiting() })
-self.addEventListener('activate', function(e) { e.waitUntil(clients.claim()) })
+self.addEventListener('install', function() {
+  self.skipWaiting()
+})
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    Promise.all([
+      clients.claim(),
+      caches.keys().then(function(ks) { return Promise.all(ks.map(function(k) { return caches.delete(k) })) }),
+    ])
+  )
+})

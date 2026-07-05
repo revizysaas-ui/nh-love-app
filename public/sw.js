3fdc54455eb868545849652ddd951c86ab35b005
nasriibrahim2007 <nasriibrahim2007@gmail.com>
2026-07-05 14:04:15 +0300

    SW v4 network-first+fallback + iOS force reload

--- public/sw.js
+++ public/sw.js
@@ -1,22 +1,27 @@
-const CACHE = 'nh-cache-v3'
+const CACHE = 'nh-cache-v4'
 const ASSETS = ['/manifest.json', '/icon-192.svg', '/icon-512.svg']
 
 self.addEventListener('install', e => {
+  self.skipWaiting()
   e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
   )
 })
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+    Promise.all([
+      caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))),
+      clients.claim(),
+    ])
   )
-  e.waitUntil(clients.claim())
 })
 
 self.addEventListener('fetch', e => {
   if (e.request.mode === 'navigate') {
-    e.respondWith(fetch(e.request))
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match(e.request))
+    )
     return
   }
   e.respondWith(
